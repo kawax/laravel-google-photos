ca47c4fd6bf9213f74d06d7fc9a5e29d3a49d787
kawax <kawaxbiz@gmail.com>
2024-10-01 10:59:43 +0900

    Update PhotosClient.php

--- src/PhotosClient.php
+++ src/PhotosClient.php
@@ -2,20 +2,25 @@
 
 namespace Revolution\Google\Photos;
 
+use Google\ApiCore\ApiException;
+use Google\ApiCore\PagedListResponse;
+use Google\ApiCore\RetrySettings;
 use Google\ApiCore\ValidationException;
 use Google\Auth\Credentials\UserRefreshCredentials;
+use Google\Photos\Library\V1\AlbumPosition;
+use Google\Photos\Library\V1\BatchCreateMediaItemsResponse;
+use Google\Photos\Library\V1\Filters;
 use Google\Photos\Library\V1\PhotosLibraryClient;
+use Google\Photos\Library\V1\PhotosLibraryResourceFactory;
 use Illuminate\Support\Traits\Conditionable;
 use Illuminate\Support\Traits\ForwardsCalls;
 use Illuminate\Support\Traits\Macroable;
-use Revolution\Google\Photos\Concerns\WithMediaItems;
 use Revolution\Google\Photos\Contracts\Factory;
 use Revolution\Google\Photos\Support\Token;
 use RuntimeException;
 
 class PhotosClient implements Factory
 {
-    use WithMediaItems;
     use Conditionable;
     use ForwardsCalls;
     use Macroable {
@@ -56,6 +61,37 @@ class PhotosClient implements Factory
         $client = new PhotosLibraryClient(['credentials' => $credentials]);
 
         return $this->setService($client);
+    }
+
+    /**
+     * mediaItems.search.
+     *
+     * @param  array{albumId?: string, pageSize?: integer, pageToken?: string, filters?: Filters, orderBy?: string, retrySettings?: RetrySettings|array}  $optionalArgs
+     *
+     * @throws ApiException
+     */
+    public function search(array $optionalArgs = []): PagedListResponse
+    {
+        return $this->getService()->searchMediaItems($optionalArgs);
+    }
+
+    /**
+     * mediaItems.batchCreate.
+     *
+     * @param  array<string>  $uploadTokens
+     * @param  array{albumId?: string, albumPosition?: AlbumPosition, retrySettings?: RetrySettings|array}  $optionalArgs
+     *
+     * @throws ApiException
+     */
+    public function batchCreate(array $uploadTokens, array $optionalArgs = []): BatchCreateMediaItemsResponse
+    {
+        $newMediaItems = [];
+
+        foreach ($uploadTokens as $token) {
+            $newMediaItems[] = PhotosLibraryResourceFactory::newMediaItem($token);
+        }
+
+        return $this->getService()->batchCreateMediaItems($newMediaItems, $optionalArgs);
     }
 
     /**
